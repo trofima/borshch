d1666bfd2f3d1931b66a187bca3e35d3dc685336
Serhii Trofimets <azarija@gmail.com>
2023-01-07 04:58:14 +0200

    cleanup

--- src/deprecated/router/notFoundRoute/index.js
+++
@@ -1,8 +0,0 @@
-import render from './template.ejs';
-import BaseRoute from '../common/BaseRoute.js';
-
-export default class BorshchRouteNotFound extends BaseRoute {
-  render() {
-    return render();
-  }
-}
