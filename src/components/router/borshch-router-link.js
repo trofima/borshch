3b2c14af34d0e06cbbd0e1d2145307935da445fb
Serhii Trofimets <azarija@gmail.com>
2023-01-09 23:56:07 +0200

    rename

---
+++ src/components/router/borshch-router-link.js
@@ -0,0 +1,36 @@
+import render from './borshch-router-link.ejs'
+import {mixin, removeEndingSlash} from '../../utilities'
+import {BorshchComponent, ReflectAttributes} from '../common'
+import borshchRouterManager from './borshch-router-manager-instance'
+
+class BorshchRouterLink extends mixin(
+  BorshchComponent,
+  ReflectAttributes('active', {name: 'path', stringify: removeEndingSlash}),
+) {
+  constructor(...args) {
+    super(...args)
+    this.path = removeEndingSlash(this.path)
+  }
+
+  render() {
+    return render({href: this.path})
+  }
+
+  onConnected() {
+    super.onConnected()
+    this.addEventListener('click', this.#navigate)
+    borshchRouterManager.on('pathChange', this.#setActive)
+    this.host.querySelector('a').addEventListener('click', e => e.preventDefault())
+  }
+
+  onDisconnected() {
+    super.onDisconnected()
+    this.removeEventListener('click', this.#navigate)
+    borshchRouterManager.off('pathChange', this.#setActive)
+  }
+
+  #navigate = () => borshchRouterManager.navigate(this.path)
+  #setActive = ({nextPath}) => this.active = nextPath === this.path
+}
+
+ export default BorshchRouterLink.define()
