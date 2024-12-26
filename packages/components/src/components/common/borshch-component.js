f02256269cb0eb51d35eb0da00cde18070ad96c6
trofima <azarija@gmail.com>
2024-12-27 01:13:01 +0200

    removed shadycss

--- packages/components/src/components/common/borshch-component.js
+++ packages/components/src/components/common/borshch-component.js
@@ -16,19 +16,11 @@ export default class BorshchComponent extends BorshchElementOf(HTMLElement) {
   attach(html) {
     const tpl = document.createElement('template')
     tpl.innerHTML = html
-    this.#applyShadyCss(tpl)
     this.#host.appendChild(tpl.content.cloneNode(true))
   }
 
   render() {
     return ''
-  }
-
-  #applyShadyCss(tpl) {
-    if (window.ShadyCSS) { //if browser doesn't support style scoping in shadow dom
-      window.ShadyCSS.prepareTemplate(tpl, this.constructor.componentName)
-      window.ShadyCSS.styleElement(this)
-    }
   }
 
   #host
